06f17b4fc740303a7d2def11bf416cd7a1b541ae
Darren Cauthon <darren@cauthon.com>
2015-05-08 13:26:22 -0500

    Refactor.

--- lib/lot/types/relation.rb
+++ lib/lot/types/relation.rb
@@ -1,7 +1,12 @@
 module Lot
   module Relation
     def self.serialize input
-      { record_uuid: input.record_uuid, id: input.id, name: input.name, record_type: input.record_type }
+      {
+        record_uuid: input.record_uuid,
+        id:          input.id,
+        name:        input.name,
+        record_type: input.record_type
+      }
     end
   end
 end
