fbdf25b4aceace288298035881444c3e9a6d10f3
Darren Cauthon <darren@cauthon.com>
2015-07-05 14:07:20 -0500

    Refactor.

--- lib/lot/event.rb
+++ lib/lot/event.rb
@@ -1,11 +1,13 @@
 module Lot
+
   module Event
+
     def self.publish event, data
-      Lot::EventHandler.types.each do |type|
-        if type.subscribed?(event, data)
-          type.fire event, data
-        end
-      end
+      Lot::EventHandler.types
+        .select { |t| t.subscribed? event, data }
+        .each   { |t| t.fire event, data }
     end
+
   end
+
 end
